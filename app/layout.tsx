7b2e8d28723ef1bfb4ed5b27e67852b4af85d05a
Adam Gill <adam.douglas.gill@gmail.com>
2025-07-11 17:35:12 -0400

    fixed build errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./globals.scss";
 import { cn } from "@/lib/utils";
 
 import { Space_Grotesk } from "next/font/google";
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,7 @@
-import Tiptap from "@/components/tipTap";
-
 export default function Test() {
   return (
     <div className="w-full h-full">
       <h1>Test</h1>
-      <Tiptap />
     </div>
   )
 }
