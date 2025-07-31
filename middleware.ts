11738d019a10333fd7ed98558019210d30488322
adam-gill <adam.douglas.gill@gmail.com>
2025-07-31 17:18:20 -0400

    edit middleware

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,32 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-const protectedRoutes = ['/dashboard']
+const protectedRoutes = ["/dashboard"];
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
-  
-  const isProtectedRoute = protectedRoutes.some(route => 
+  const { pathname } = request.nextUrl;
+  console.log(pathname);
+
+  const isProtectedRoute = protectedRoutes.some((route) =>
     pathname.startsWith(route)
-  )
-  
+  );
+
   if (isProtectedRoute) {
-    const sessionToken = request.cookies.get('better-auth.session_token')
-    
+    const sessionToken = request.cookies.get("better-auth.session_token");
+    console.log(sessionToken);
+
     if (!sessionToken) {
-      return NextResponse.redirect(new URL('/', request.url))
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (pathname === "/") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
     }
   }
-  
-  return NextResponse.next()
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
-}
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
