2d2deed36880a1e2243ce71f2d146fc98a4d45d7
Adam Gill <adam.douglas.gill@gmail.com>
2025-09-07 17:20:28 -0400

    Update noteWrapper.tsx

--- app/u/components/noteWrapper.tsx
+++ app/u/components/noteWrapper.tsx
@@ -149,7 +149,7 @@ export default function NoteWrapper({
 
   return (
     <>
-      {content ? (
+      {content !== undefined ? (
         <SimpleEditor
           content={content}
           setContent={handleContentChange}
