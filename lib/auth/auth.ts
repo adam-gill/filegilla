13fe328bf9a716aeba8bdbed8392740b33167051
Adam Gill <adam.douglas.gill@gmail.com>
2025-10-16 07:47:05 -0400

    make session longer

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -50,5 +50,9 @@ export const auth = betterAuth({
       },
     },
   },
+  session: {
+    expiresIn: 60 * 60 * 24 * 30, // 30 days
+    updateAge: 60 * 60 * 24, // 1 day
+  },
   plugins: [jwt()],
 });
