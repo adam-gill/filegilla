96893f28932488d3a8111101671df57ced9ea804
Adam Gill <adam.douglas.gill@gmail.com>
2025-11-02 22:50:48 -0500

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const protectedRoutes = ["/u", "/test", "/note", "/posts"];
+const protectedRoutes = ["/u", "/test", "/note"];
 
 export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
